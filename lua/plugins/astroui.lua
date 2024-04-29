546951fd0a121751b232f68b8b0b99169f974eda
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2024-04-29 22:42:24 +0200

    Add collections and make it work with DARK_THEME GNOME variable

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -7,20 +7,13 @@
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
-  opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
-    },
-    -- Icons can be configured throughout the interface
-    icons = {
+  opts = function(_, opts)
+    if vim.env.DARK_THEME == "1" then
+      opts.colorscheme = "catppuccin-mocha"
+    else
+      opts.colorscheme = "catppuccin-latte"
+    end
+    opts.icons = {
       -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
@@ -32,6 +25,19 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
-    },
-  },
+    }
+  end,
+  -- opts = {
+  -- change colorscheme
+  -- colorscheme = "catppuccin-mocha",
+  -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+  -- highlights = {
+  --  init = { -- this table overrides highlights in all themes
+  -- Normal = { bg = "#000000" },
+  --  },
+  --  astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
+  -- Normal = { bg = "#000000" },
+  --  },
+  -- },
+  -- Icons can be configured throughout the interface
 }
